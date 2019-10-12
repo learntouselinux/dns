a43be1a31d1cb958d5cd0a49b33ac8ae93e76ee9
Brian <brian.musial@gmail.com>
2019-10-11 18:44:06 -0700

    added white space after domain definition

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "digitalocean_domain" "root" {
 
 #Add A record to the domain
 resource "digitalocean_record" "root" {
-  domain ="${digitalocean_domain.root.name}"
+  domain = "${digitalocean_domain.root.name}"
   type = "A"
   name = "@"
   value = "159.65.65.52"  
