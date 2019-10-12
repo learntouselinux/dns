18387533f496a9aad227cdc6e005e293c20d928c
Brian <brian.musial@gmail.com>
2019-10-11 18:36:48 -0700

    changed spacing on line 8

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "digitalocean" {}
 resource "digitalocean_domain" "root" {
   name = "learntouselinux.com"
 } 
+
 #Add A record to the domain
-  resource "digitalocean_record" "root" {
+resource "digitalocean_record" "root" {
   domain ="${digitalocean_domain.root.name}"
   type = "A"
   name = "@"
