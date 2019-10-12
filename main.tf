05ed14f0d001074d66ca4f828d721dd7e9178c0a
Brian <brian.musial@gmail.com>
2019-10-11 18:34:17 -0700

    fixed white space in code

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ provider "digitalocean" {}
 resource "digitalocean_domain" "root" {
   name = "learntouselinux.com"
 } 
-
 #Add A record to the domain
   resource "digitalocean_record" "root" {
   domain ="${digitalocean_domain.root.name}"
